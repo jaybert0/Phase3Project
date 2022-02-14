093937212cbbadf8aadfb816567a5946a7a5f8c2
J Lee <jay.j.lee.1@gmail.com>
2022-02-14 16:46:58 -0500

    seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -2,6 +2,34 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 Maker.create(
-    username: Faker::Name.name
+    username: "Head Setter"
 )
+User.create(
+    username: "SpiderMan"
+)
+
+15.times { 
+    Climbproblem.create(
+        favorite:Faker::Boolean.boolean,
+        in_progress:Faker::Boolean.boolean,
+        completed:Faker::Boolean.boolean,
+        climber_feedback:Faker::TvShows::Simpsons.quote,
+        route_rating:rand(0..5.0),
+        user_id:1
+        problem_id:Problem.all.sample.id
+    )    
+  }
+
+  15.times { 
+      Problem.create(
+          difficulty:rand(0..16),
+          grip_color:Faker::Color.color_name,
+          technique:Faker::Kpop.i_groups,
+          location:rand(0..10),
+          end_date:2022-03-15,
+          problem_description:Faker::TvShows::Seinfeld.quote,
+          maker_id:1
+      )
+   }
+
 puts "✅ Done seeding!"
