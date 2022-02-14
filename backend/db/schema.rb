2170691ca77e1d7ad540c736ded809f619a61c3e
Frederick <admin@freddyscovell.com>
2022-02-14 12:49:14 -0500

    schema has been created

---
+++ backend/db/schema.rb
@@ -0,0 +1,48 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2022_02_14_174129) do
+
+  create_table "betas", force: :cascade do |t|
+    t.boolean "favorite"
+    t.boolean "in_progress"
+    t.boolean "completed"
+    t.string "climber_feedback"
+    t.integer "climber_id"
+    t.integer "problem_id"
+    t.float "route_rating"
+    t.index ["climber_id"], name: "index_betas_on_climber_id"
+    t.index ["problem_id"], name: "index_betas_on_problem_id"
+  end
+
+  create_table "climbers", force: :cascade do |t|
+    t.string "username"
+  end
+
+  create_table "problems", force: :cascade do |t|
+    t.integer "difficulty"
+    t.string "grip_color"
+    t.string "technique"
+    t.integer "location"
+    t.date "end_date"
+    t.string "problem_description"
+    t.integer "route_setter_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["route_setter_id"], name: "index_problems_on_route_setter_id"
+  end
+
+  create_table "route_setters", force: :cascade do |t|
+    t.string "username"
+  end
+
+end
