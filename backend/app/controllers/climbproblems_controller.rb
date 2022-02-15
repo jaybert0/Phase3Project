c65ae96bafe8c3e413a5b7b5a9635bf4eb333035
J Lee <jay.j.lee.1@gmail.com>
2022-02-15 10:47:27 -0500

    backend methods updated

--- backend/app/controllers/climbproblems_controller.rb
+++ backend/app/controllers/climbproblems_controller.rb
@@ -1,15 +1,32 @@
 require 'pry'
 class ClimbproblemsController < ApplicationController
+    get '/climbproblems' do
+        problems = Climbproblem.all
+        problems.to_json(
+            only: [:id, :favorite, :in_progress, :completed, :climber_feedback, :problem_id, :route_rating]
+        )
+    end
 
+    get '/climbproblems/:id' do
+        problems = Climbproblem.find(params[:id])
+        problems.to_json(
+            only: [:id, :favorite, :in_progress, :completed, :climber_feedback, :problem_id, :route_rating]
+        )
+    end
 
-    # I don't fully understand how to call functions using the controller,so I have a bunch of methods in here that I need to trigger or run from post and patch prompts
-    # post '/climbproblem/:id/:climber_feedback' do
-    # climb_problem = Climbproblem.find(params[:id])
-    # climb_problem.create(
-    #   climber_feedback: params[:climber_feedback]
-    # )
-    # climb_problem.to_json
-    # end
+    post '/climbproblem' do
+        
+        climb_problem = Climbproblem.create(
+            favorite: params[:favorite],
+            in_progress: params[:in_progress],
+            completed: params[:completed],
+            climber_feedback: params[:climber_feedback],
+            user_id: User.last.id,
+            problem_id: params[:problem_id],
+            route_rating: params[:route_rating]
+        )
+        climb_problem.to_json
+    end
     # I am a little unsure how we would differentiate between the two of these if it were just tied to the ID and patch requests
     # def give_climber_feedback(feedback)
     #     binding.pry
@@ -39,6 +56,18 @@ class ClimbproblemsController < ApplicationController
     #     binding.pry
     #     self.in_progress = !self.in_progress
     # end
+
+    patch '/climbproblems/:id' do
+        problem = Climbproblem.find(params[:id])
+        problem.update(
+          in_progress: params[:in_progress],
+          favorite: params[:favorite],
+          completed: params[:completed],
+          climber_feedback: params[:climber_feedback],
+          route_rating: params[:route_rating]
+        )
+        problem.to_json
+    end
 
     # patch '/climbproblem/:id/:is_completed' do  
     # def is_completed
