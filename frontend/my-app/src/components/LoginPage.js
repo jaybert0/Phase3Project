c644e0c5a8debad41bb7a0dc9e239d56fd16fae8
J Lee <jay.j.lee.1@gmail.com>
2022-02-16 10:00:37 -0500

    new login page

--- frontend/my-app/src/components/LoginPage.js
+++ frontend/my-app/src/components/LoginPage.js
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from 'react';
+import Grid from "@material-ui/core/Grid";
+import Box from "@mui/material/Box";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
 function LoginPage(){
     return(
         <div>
-
-            <h1>"sent to LoginPage"</h1>
+        <Grid container justify="center">
+            <h1>Welcome! Are you a:</h1>
+        </Grid>
+        <Grid container justify="center">
+            <Stack spacing={2} direction="row">
+                <Button variant="contained" component={Link} to="/boltmonkey">Route Setter</Button>
+                <Button variant="contained" component={Link} to="/climberlogin">Climber</Button>
+           </Stack>
+        </Grid>
+            
             
         </div>
         
