647766e75f19d93a68b6135f70596d247ac523be
J Lee <jay.j.lee.1@gmail.com>
2022-02-17 14:34:45 -0500

    makercard ready to test

--- frontend/my-app/src/components/MakerHome.js
+++ frontend/my-app/src/components/MakerHome.js
@@ -5,6 +5,16 @@ function MakerHome({problem, climbproblem}){
 
             <h1>"sent to MakerHome"</h1>
             <ProblemForm problem={problem} climbProblem={climbproblem}/>
+            {problem.map(problem => (
+                <MakerCard 
+                key={problem.id} 
+                difficulty={problem.difficulty}
+                location={problem.location}
+                technique={problem.technique}
+                grip_color={problem.grip_color}
+                end_date={problem.end_date}
+                problem_description={problem.problem_description}
+                />
         </div>
         
     );
